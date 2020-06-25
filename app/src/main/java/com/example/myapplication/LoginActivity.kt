4a5ce9fcd6cf674eb43cc6aef4b363615aa4cd6d
bazsup <supawit.ruen@mail.kmutt.ac.th>
2020-06-25 15:01:39 +0700

    :tada: Init my first android project :basketball:

---
+++ app/src/main/java/com/example/myapplication/LoginActivity.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication
+
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+
+    val positiveButtonClick = { dialog: DialogInterface, which: Int ->
+        Toast.makeText(applicationContext, android.R.string.ok, Toast.LENGTH_SHORT).show()
+    }
+
+    val negativeButtonClick = { dialog: DialogInterface, which: Int ->
+        Toast.makeText(applicationContext, android.R.string.cancel, Toast.LENGTH_SHORT).show()
+    }
+
+    fun onClickBtn(v: View?) {
+        Toast.makeText(this, "Clicked on Button", Toast.LENGTH_LONG).show()
+        val builder = AlertDialog.Builder(this)
+
+        with(builder)
+        {
+            setTitle("Logged in")
+            setMessage("Welcome, Bazsup!")
+            setPositiveButton("OK", DialogInterface.OnClickListener(function = positiveButtonClick))
+            setNegativeButton(android.R.string.cancel, negativeButtonClick)
+            show()
+        }
+    }
+}
